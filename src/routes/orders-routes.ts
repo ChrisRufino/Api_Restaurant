d54a85dda77f20f5677b3293c20825e4286e887e
ChriRufino <christisael@gmail.com>
2026-04-13 15:55:14 -0300

    orders-Finish

--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -4,6 +4,10 @@ import { OrdersController } from '@/controllers/orders-controller';
 const ordersRoutes = Router();
 const ordersController = new OrdersController();
 ordersRoutes.post('/', ordersController.create);
+ordersRoutes.get(
+  '/table-session/:table_session_id/total',
+  ordersController.show,
+);
 ordersRoutes.get('/table-session/:table_session_id', ordersController.index);
 
 export { ordersRoutes };
